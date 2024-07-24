8ef03eb39648b701dc5e267d35e324220cef6a95
Carlos J. Dommar <carlos.dommar@gmail.com>
2024-07-24 16:56:14 +0200

    End of day commit

---
+++ submit_job_from_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=R-simulation       # Job name
+#SBATCH --cpus-per-task=8             # CPUs
+#SBATCH --mem=128gb                   # Job memory request
+#SBATCH --time=24:0:0                 # Time limit
+
+#Rscript -e "rmarkdown::render('~/carlos-code/cervix_cancer_microsimulation--20240617T075924Z-001/cervix_cancer_microsimulation/Cervix_MicroSim_RMarkdown_v.071.Rmd')"
+
+
+
+Rscript /home/07075107P/carlos-code/cervix_cancer_microsimulation--20240617T075924Z-001/cervix_cancer_microsimulation/cervix_microSim_stacked_list.R      # Run your R script
