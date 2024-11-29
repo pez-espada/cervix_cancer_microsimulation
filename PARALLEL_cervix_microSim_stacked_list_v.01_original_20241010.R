75eb10584eaf5b39772a66182882e1139dcf8028
Carlos Javier Dommar D Lima|cdommar@idibell.cat <carlos.dommar@gmail.com>
2024-11-29 15:01:15 +0100

    a regular  commit

--- PARALLEL_cervix_microSim_stacked_list_v.01_original_20241010.R
+++ PARALLEL_cervix_microSim_stacked_list_v.01_original_20241010.R
@@ -901,7 +901,7 @@ return(stacked_results)
 Sys.setenv(OMP_NUM_THREADS = "1") # to prevent conflicts between OpenMP and R parallel
 p = Sys.time()
 # run for no treatment
-numb_of_sims = 20
+numb_of_sims = 80
 sim_no_trt  <- MicroSim(strategy = "natural_history", numb_of_sims = numb_of_sims, 
                       v_M_1 = v_M_1, n_i = n_i, n_t = n_t, v_n = v_n, 
                       d_c = d_c, d_e = d_e, TR_out = TRUE, TS_out = TRUE, 
@@ -1308,14 +1308,13 @@ other_mean_mortality_result <-
 other_mean_mortality_func(sim_stalked_result = 
                               other_mean_mortality_result, my_Probs = my_Probs)  
 
-## save the results
-##saveRDS(object = other_mean_mortality_result, file = "./data/stacked_sims_20x5x10E5x75_20241127_madeinPADO_TEST_from_script_3.rds")
-#saveRDS(object = other_mean_mortality_result, file = 
-#          "./data/stacked_sims_20x10E6x75_20241129_madeinPADO_TEST_from_script_7.rds")
+# save the results
+#saveRDS(object = other_mean_mortality_result, file = "./data/stacked_sims_20x5x10E5x75_20241127_madeinPADO_TEST_from_script_3.rds")
+saveRDS(object = other_mean_mortality_result, file = "./data/stacked_sims_80x10E6x75_20241128_madeinPADO_TEST_from_script_5.rds")
 
-## Load the results , if they have produced previously (comment the above lines)
 #other_mean_mortality_result <-
-#  readRDS(file = "./data/stacked_sims_20x10E6x75_20241129_madeinPADO_TEST_from_script_6.rds")
+#  readRDS(file = "./data/stacked_sims_20x10E6x75_20241128_madeinPADO_TEST_from_script_4.rds")
+
 
 
 ### ----Convert .Rmd to .R
