1e80d6807d7e0c975bc85658e4e618d41810e2ab
pez-espada <carlos.dommar@gmail.com>
2025-02-17 21:15:51 +0100

    Evening commit before going to bed. Work in progress, broken code. Tomorroe more

--- PARALLEL_cervix_microSim_stacked_list_v.01_original_20241010.R
+++ PARALLEL_cervix_microSim_stacked_list_v.01_original_20241010.R
@@ -1148,6 +1148,7 @@ MicroSim <- function(strategy="natural_history", numb_of_sims = 20,
       
       # Combine stored entries in a single data frame
       symptomatics <- bind_rows(stored_list)
+      cat("symprotamics dimensions: ", dim(symptomatics),"\n")
       tc_disc <- m_C[,1:n_t] %*% v_dwc       # total (discounted) cost per individual
       te_disc <- m_E[,1:n_t] %*% v_dwe       # total (discounted) QALYs per individual 
       
@@ -1466,8 +1467,8 @@ clusterExport(cl, c("Costs_per_Cancer_Diag", "Effs", "trans_prb", "Probs",
                     "update_column", "states_to_check", 
                     "symptom_prob_vec", "survival_prob_vec", #"global_diagnosed", 
                     "cost_Vec", "new_cases_2"))
-registerDoParallel(cl) # for parallel
-#registerDoSEQ()        # for sequential
+#registerDoParallel(cl) # for parallel
+registerDoSEQ()        # for sequential
 ################################################################################
 ################################################################################
 
